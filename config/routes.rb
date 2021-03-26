93e4269de84fa07f6b8e1492f0c2b3d9768e6ff4
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2021-03-26 13:19:46 -0300

    feat: templetas added and areas user and admins configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :profiles
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
   devise_for :admins
-  get 'welcome/index'
+  devise_for :users
+  
+  get 'site/welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
 end
